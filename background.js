e2bf66cb6d747e37e32131a962bb96d1e030c918
ayush4059 <ayush.agrawal@livspace.com>
2018-02-21 18:51:35 +0530

    youtube search added

--- background.js
+++ background.js
@@ -1,3 +1,40 @@
 chrome.runtime.onMessage.addListener(function (msg, sender) {
-	chrome.browserAction.setBadgeText({text: msg.imdbRating});
+	if (msg.badge == 1)
+		chrome.browserAction.setBadgeText({text: msg.imdbRating});
 });
+
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+	if (msg.title != undefined) {
+		var request = gapi.client.request({
+			method: 'GET',
+			path: '/youtube/v3/search',
+			params: {
+				part: 'snippet',
+	            type: 'video',
+	            q: msg.title + ' trailer',
+	            maxResult: 5,
+	            order: 'viewCount'
+			}
+		});
+
+		request.execute(function (response) {
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			    var activeTab = tabs[0];
+			    chrome.tabs.sendMessage(activeTab.id, {'response': response, 'title': msg.title});
+			});
+		});
+	}
+});
+
+function init () {
+	gapi.client.setApiKey('AIzaSyAdToc91_oFiu3ZHZyiNwqyc0qF7dxZ3Cs');
+	gapi.client.load('youtube', 'v3', function () {
+		// api ready
+	});
+}
+
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "https://apis.google.com/js/client.js?onload=init";
+head.appendChild(script);
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-$(document).on("click", function () {
-	var title = $('.title-card.is-focused').find('.video-preload-title-label').text();
-	var tile  = $('.title-card.is-focused');
+$(document).on('click', '.slider-item', function () {
+	var title = $(this).find('.video-preload-title-label').text();
+	var element = $(this);
 	var manifest = chrome.runtime.getManifest();
 	if (title != undefined) {
 		$.ajax({
@@ -9,22 +9,40 @@ $(document).on("click", function () {
 		}).done(function(result){
 			if (result.imdbRating == undefined)
 				result.imdbRating = 'N/A';
+
 			// show rating on extension as badge
 			if (manifest.type.badge == 1) {
-				chrome.runtime.sendMessage({'imdbRating': result.imdbRating});	
+				chrome.runtime.sendMessage({'badge': 1, 'imdbRating': result.imdbRating});	
 
 			// show rating on top of movie tile on netflix
 			} else {
-				var div = '<div class="imdb-rating" style="width: 40px;height: 17px;position: absolute;z-index: 1000;'
-					+ 'top: -25px;left: 103px;border-radius: 20px;text-align: center;background: #000;'
-					+ 'background-color: #235bc3;">' + result.imdbRating
+				chrome.runtime.sendMessage({'title': title});
+				var div = '<div class="imdb-rating" style="width: 103px;position: absolute;z-index: 1000;'
+					+ 'top: -49px;left: 80px;border-radius: 5px;text-align: center;background: #000;'
+					+ 'background-color: #235bc3;padding: 4px;">IMDB rating - ' + result.imdbRating
 					+ '<div style="border-left: 4px solid transparent;'
 					+ 'border-right: 4px solid transparent;border-top: 8px solid #235bc3;'
-					+ 'position: absolute;left: 16px;top: 14px;"></div></div>';
-				var parent = tile.closest('.slider-item');
-				parent.find('.imdb-rating').remove();
-				parent.prepend(div);
+					+ 'position: absolute;left: 52px;top: 41px;"></div></div>';
+				element.find('.imdb-rating').remove();
+				element.prepend(div);
 			}
 		});
 	}
+});
+
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+	var response = msg.response.items;
+	var result;
+	for (var i = 0; i < 5; i++) {
+		var title = response[i].snippet.title;
+
+		if (title.indexOf("trailer") != -1 || title.indexOf("Trailer") != -1) {
+			result = response[i];
+			if (title.indexOf(msg.title) != -1) {
+				break;	
+			}
+		}
+	}
+	var html = '<a style="display: block;text-decoration: underline;margin-bottom: 3px;" target="_blank" href="https://www.youtube.com/watch?v=' + result.id.videoId + '">Watch Trailer</a>';
+	$('.title-card.is-focused').closest('.title-card-container').siblings('.imdb-rating').prepend(html);
 });
